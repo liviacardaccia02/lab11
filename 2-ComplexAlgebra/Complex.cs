7e70fb1be8e822662ef4d4cba101043d78d5289f
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2022-12-05 13:17:16 +0100

    exercise 2-ComplexAlgebra completed

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,4 +1,6 @@
+using System;
 namespace ComplexAlgebra
+
 {
     /// <summary>
     /// A type for representing Complex numbers.
@@ -17,6 +19,61 @@ namespace ComplexAlgebra
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public Complex(double real, double immaginary) 
+        {
+            Real = real;
+            Imm = immaginary;
+        }
+
+        public double Real { get; }
+
+        public double Imm { get; }
+
+        public double Modulus => Math.Sqrt(Real * Real + Imm * Imm);
+
+        public double Phase => Math.Atan2(Imm, Real);
+
+        public Complex Complement()
+        {
+            return new Complex(Real, -Imm);
+        }
+
+        public Complex Sum(Complex complex)
+        {
+            return new Complex(Real + complex.Real, Imm + complex.Imm);
+        }
+
+        public Complex Sub(Complex complex)
+        {
+            return new Complex(Real - complex.Real, Imm - complex.Imm);
+        }
+
+        public override string ToString()
+        {
+            if (Real == 0) 
+            {
+                return Imm == 0 ? "0" : $"{Imm}i";
+            }
+
+            return Imm >= 0 ? $"{ Real } + { Imm }i" : $"{ Real } { Imm }i";
+        }
+
+        public override bool Equals(object obj) 
+        {
+            Complex complex = obj as Complex;
+
+            if (complex == null) 
+            {
+                return false;
+            }
+
+            if (Real == complex.Real && Imm == complex.Imm) 
+            {
+                return true;
+            }
+
+            return false;
+        }
+
     }
 }
